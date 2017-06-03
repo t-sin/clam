6d9c08011995bf2226b5d47210f2139bb1ca15be
t-sin <shinichi.tanaka45@gmail.com>
2017-06-03 15:00:05 +0900

    Tokenizes input

--- main.lisp
+++ main.lisp
@@ -9,16 +9,36 @@
   (format *standard-output* "~&clamshell$ ")
   (finish-output *standard-output*))
 
+(defvar *clam-delimiter* #(#\space #\tab))
+
+(defun tokenize (line)
+  (let ((tokens (make-array 1 :adjustable t :fill-pointer 0))
+        (buffer  (make-string-output-stream)))
+    (labels ((store-token (token)
+               (when (plusp (length token))
+                 (vector-push-extend token tokens)))
+             (for-char (c)
+               (if (position c *clam-delimiter*)
+                   (progn
+                     (store-token (get-output-stream-string buffer))
+                     (setf buffer (make-string-output-stream)))
+                   (write-char c buffer))))
+      (loop
+         :for c :across line
+         :do (for-char c)
+         :finally (store-token (get-output-stream-string buffer))
+         (return-from tokenize tokens)))))
+
 (defun clam-read ()
   (show-prompt)
-  (read-line))
+  (tokenize (read-line)))
 
 (defun clam-eval (args)
   args)
 
 (defun clam-print (object)
   (progn
-    (format *standard-output* "~a" object)
+    (format *standard-output* "~s" object)
     (finish-output *standard-output*)
     object))
 
