332d411defcf44edd84e8cc95a3c9e74b5bb4b01
t-sin <shinichi.tanaka45@gmail.com>
2017-06-03 18:46:57 +0900

    Simple quotation

--- main.lisp
+++ main.lisp
@@ -10,19 +10,29 @@
   (finish-output *standard-output*))
 
 (defvar *clam-delimiter* #(#\space #\tab))
+(defvar *clam-quotes* #(#\' #\"))
 
 (defun tokenize (line)
   (let ((tokens (make-array 1 :adjustable t :fill-pointer 0))
-        (buffer  (make-string-output-stream)))
+        (buffer  (make-string-output-stream))
+        (quote-char nil))
     (labels ((store-token (token)
                (when (plusp (length token))
                  (vector-push-extend token tokens)))
              (for-char (c)
-               (if (position c *clam-delimiter*)
-                   (progn
-                     (store-token (get-output-stream-string buffer))
-                     (setf buffer (make-string-output-stream)))
-                   (write-char c buffer))))
+               (cond ((position c *clam-delimiter*)
+                      (if quote-char
+                          (write-char c buffer)
+                          (progn
+                            (store-token (get-output-stream-string buffer))
+                            (setf buffer (make-string-output-stream)))))
+                     ((and quote-char (char= c quote-char))
+                      (setf quote-char nil)
+                      (write-char c buffer))
+                     ((position c *clam-quotes*)
+                      (setf quote-char c)
+                      (write-char c buffer))
+                     (t (write-char c buffer)))))
       (loop
          :for c :across line
          :do (for-char c)
